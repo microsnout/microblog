7c92e8ee2da14ba927616aee910d05f0221a1275
Barry <microsnout@gmail.com>
2020-12-10 16:51:39 -0500

    comment gravatars

---
+++ blog/templatetags/filter.py
@@ -0,0 +1,20 @@
+import hashlib
+import urllib
+from django import template
+from django.utils.safestring import mark_safe
+ 
+register = template.Library()
+ 
+# return only the URL of the gravatar
+# TEMPLATE USE:  {{ email|gravatar_url:150 }}
+@register.filter
+def gravatar_url(email, size=40):
+  default = "https://bulma.io/images/placeholders/128x128.png"
+  return "https://www.gravatar.com/avatar/%s?%s" % (hashlib.md5(email.encode("utf-8").lower()).hexdigest(), urllib.parse.urlencode({'d':default, 's':str(size)}))
+ 
+# return an image tag with the gravatar
+# TEMPLATE USE:  {{ email|gravatar:150 }}
+@register.filter
+def gravatar(email, size=40):
+    url = gravatar_url(email, size)
+    return mark_safe('<img src="%s" height="%d" width="%d">' % (url, size, size))
--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.http import HttpResponse
-from .forms import EmailPostForm
 from django.core.mail import send_mail
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
-from .models import Post
+# Debug logging only
+import logging
+logger = logging.getLogger(__name__)
 
 app_name = 'blog'
 
-#def detail(request, year, month, day, post):
-#    post = get_object_or_404(Post, slug=post,
-#                                   status='published',
-#                                   publish__year=year,
-#                                   publish__month=month,
-#                                   publish__day=day)
-#    others = Post.published.all() \
-#                 .exclude(id=post.id)
-#    return render(request,
-#                  'blog/post/detail.html',
-#                  {'post': post,
-#                   'others': others})
-
-def detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post,
-                                   status='published',
-                                   publish__year=year,
-                                   publish__month=month,
-                                   publish__day=day)
-    others = Post.published.all() \
-                 .exclude(id=post.id)
-    sent = False
-    modal = False
-    cd = []
-    if request.method == 'POST':
-        # Form was submitted
+def add_email_modal(request, post, context):
+    if request.method == 'POST' and \
+        'send-email' in request.POST:
+        # Form was submitted, need to send email
+        logger.debug("add_email_modal: POST")
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
@@ -45,18 +27,72 @@ def detail(request, year, month, day, post):
                       f"{post.title}\n" \
                       f"{post_url}\n" 
             #send_mail(subject, message, 'microsnout@bell.net', [cd['you']])
-            sent = True
-            modal = True
+            context.update({
+                'sent': True,
+                'modal': True,
+                'modal_data': cd,
+            })
     else:
+        # GET request
         form = EmailPostForm()
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post,
-                   'others': others,
-                   'sent': sent,
-                   'form': form,
-                   'modal': modal,
-                   'modal_data': cd})
+        context.update({
+            'form': form
+        })
+
+def add_new_comment(request, post, context):
+    comment = None
+    if request.method == 'POST' and \
+        'add-comment' in request.POST:
+        logger.debug("add_new_comment: POST")
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            # Create Comment obj but don't save to db
+            comment = form.save(commit=False)
+            # Assign current post to the comment
+            comment.post = post
+            # Save to db
+            comment.save()
+    else:
+        form = CommentForm()
+
+    context.update({
+        'new_comment': comment,
+        'comment_form': form
+    })
+
+def detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    
+    # Most recent 6 posts not including the detailed one
+    others = Post.published.all() \
+                 .exclude(id=post.id) \
+                 .order_by("-created")[:6]
+
+    comments = post.comments.filter(active=True)
+
+    nm = "-"
+    if 'add-comment' in request.POST:
+        nm = "AC"
+    if 'send-email' in request.POST:
+        nm = "SE"
+    logger.debug( f">>>detail: {request.method} name:{nm}" )
+
+    # The detailed post and the others
+    context = {
+        'post': post,
+        'others': others,
+        'comments': comments,
+    }
+
+    # Include form for email sharing and new Comment form 
+    add_email_modal(request, post, context)
+    add_new_comment(request, post, context)
+        
+    return render(request, 'blog/post/detail.html', context)
 
 class PostIndexView(ListView):
     queryset = Post.published.all()
